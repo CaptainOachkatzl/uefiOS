c037afd94ea3f456b97163986f850b347e627420
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-28 22:25:09 +0200

    basic memory handling finally working

--- kernel/main.c
+++ kernel/main.c
@@ -1,16 +1,22 @@
 #include <efi.h>
 #include <efilib.h>
-#include <stdlib.h>
+#include "memory/memory.h"
 
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
 {
-	InitializeLib(ImageHandle, SystemTable);
+	void * memorypageHandle;
+	void * memorypoolHandle;
 
-	Print(L"trying to allocate memory\n");
-	malloc(16);
-	Print(L"allocated successfully\n");
+	InitializeLib(ImageHandle, SystemTable);
+	
+	memorypageHandle = AllocateMemoryPages(4);
+	memorypoolHandle = AllocateMemoryPool(1024);
+
+	FreeMemoryPages(memorypageHandle, 4);
+	FreeMemoryPool(memorypoolHandle);
 
 	return EFI_SUCCESS;
 }
+
---
+++ kernel/memory/memory.h
@@ -0,0 +1,118 @@
+#include <efi.h>
+#include <efilib.h>
+#include <stdlib.h>
+
+void ExecuteMemoryMap()
+{
+	EFI_STATUS status;
+	Print(L"getting memory map\n");
+	UINTN requiredSize;
+	EFI_MEMORY_DESCRIPTOR * memoryMap = 0;
+	UINTN mapKey;
+	UINTN descriptorSize;
+	UINT32 descriptorVersion;
+	status = uefi_call_wrapper(BS->GetMemoryMap, 5, &requiredSize, memoryMap, 
+				&mapKey, &descriptorSize, &descriptorVersion);
+
+	if(status == EFI_BUFFER_TOO_SMALL)
+	{
+		Print(L"buffer too small\n");
+		return;
+	}
+	else if(status == EFI_INVALID_PARAMETER)
+	{
+		Print(L"invalid memory map\n");
+		return;
+	}
+	else
+	{
+		Print(L"got memory map.\n");
+		return;
+	}
+}
+
+void * AllocateMemoryPages(UINTN pageCount)
+{
+	EFI_STATUS status;
+	void * handle;
+	Print(L"allocating memory page\n");
+	status = uefi_call_wrapper(BS->AllocatePages, 4, AllocateAnyPages, 
+		EfiLoaderData, pageCount, &handle);
+
+	if(status == EFI_OUT_OF_RESOURCES)
+	{
+		Print(L"out of resources for page\n");
+		return 0;
+	}
+	else if(status == EFI_INVALID_PARAMETER)
+	{
+		Print(L"invalid page type\n");
+		return 0;
+	}
+	else if(status == EFI_NOT_FOUND)
+	{
+		Print(L"could not find requested pages\n");
+		return 0;
+	}
+	else
+	{
+		Print(L"memory page successfully allocated.\n");
+		return handle;
+	}
+}
+
+void * AllocateMemoryPool(UINTN poolSize)
+{
+	EFI_STATUS status;
+	void * handle;
+	Print(L"allocating memory pool\n");
+	status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, poolSize, &handle);
+
+	if(status == EFI_OUT_OF_RESOURCES)
+	{
+		Print(L"out of resources for pool\n");
+		return 0;
+	}
+	else if(status == EFI_INVALID_PARAMETER)
+	{
+		Print(L"invalid pool type\n");
+		return 0;
+	}
+	else
+	{
+		Print(L"memory pool successfully allocated.\n");
+		return handle;
+	}
+}
+
+void FreeMemoryPages(void * page, UINTN pageCount)
+{
+	EFI_STATUS status;
+	Print(L"freeing memory pages\n");
+	status = uefi_call_wrapper(BS->FreePages, 2, page, pageCount);
+
+	if(status == EFI_INVALID_PARAMETER)
+	{
+		Print(L"invalid page pointer\n");
+	}
+	else
+	{
+		Print(L"memory pages successfully freed.\n");
+	}
+}
+
+void FreeMemoryPool(void * pool)
+{
+	EFI_STATUS status;
+	Print(L"freeing memory pool\n");
+	status = uefi_call_wrapper(BS->FreePool, 1, pool);
+
+	if(status == EFI_INVALID_PARAMETER)
+	{
+		Print(L"invalid pool pointer\n");
+	}
+	else
+	{
+		Print(L"memory pool successfully freed.\n");
+	}
+}
