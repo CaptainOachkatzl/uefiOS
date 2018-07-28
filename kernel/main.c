9fe0574446a9f4703056cda2698ea4a3144a4864
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-28 13:26:49 +0200

    trying to allocate memory - without success!

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,8 @@
 #include <efi.h>
 #include <efilib.h>
 #include "console.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 EFI_STATUS
 EFIAPI
@@ -8,5 +10,19 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
 	InitializeLib(ImageHandle, SystemTable);
 	console_out(L"printing with include\n");
+
+	void * allocated;
+
+	Print(L"starting kernel v2\n");
+
+	Print(L"test allocation\n");
+	allocated = malloc(0);
+	if(allocated)
+	{
+		Print(L"test allocation successful\n");
+		free(allocated);
+	}
+
+	Print(L"test allocation failed\n");
 	return EFI_SUCCESS;
 }
