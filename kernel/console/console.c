f4d21e190bc5f11d24657dcb6a92cf6ca08ce5b7
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-30 10:36:11 +0200

    can now use formatted strings in console print

--- kernel/console/console.c
+++ kernel/console/console.c
@@ -2,13 +2,19 @@
 #include "../string/string.h"
 #include <efilib.h>
 
-void console_write(CHAR16 * string)
+void console_write(CHAR16 * string, ...)
 {
-	Print(string);
+	va_list args;
+	va_start (args, string);
+	VPrint(string, args);
+	va_end (args);
 }
 
-void console_writeline(CHAR16 * string)
+void console_writeline(CHAR16 * string, ...)
 {
-	console_write(string);
+ 	va_list args;
+	va_start (args, string);
+	VPrint(string, args);
+	va_end (args);
 	console_write(L"\n");
 }
--- kernel/console/console.h
+++ kernel/console/console.h
@@ -2,5 +2,5 @@
 
 #include <efi.h>
 
-void console_write(CHAR16 * string);
-void console_writeline(CHAR16 * string);
+void console_write(CHAR16 * string, ...);
+void console_writeline(CHAR16 * string, ...);
