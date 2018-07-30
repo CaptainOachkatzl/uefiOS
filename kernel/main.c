ae8aad67e98a0b3ebe81715bfeebe51ebb25d25e
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-30 13:20:50 +0200

    test memory is now its own function

--- kernel/main.c
+++ kernel/main.c
@@ -6,16 +6,23 @@
 #include <efi.h>
 #include <efilib.h>
 
+void test_memory();
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
 {
 	InitializeLib(ImageHandle, SystemTable);
 
+	test_memory();
+	return EFI_SUCCESS;
+}
+
+void test_memory()
+{
 	UINTN maxMemory = memory_check(0, mega(256), mega(16));
 
 	CHAR16 const * unit = convert_unit_bits_bytes(&maxMemory);
 	console_writeline(L"maximum allocated memory: %d%s", maxMemory, unit);
+}
 
-	return EFI_SUCCESS;
 }
