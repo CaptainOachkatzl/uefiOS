f6ca22d05448c8bfc5d246e2695096f5f0dc7534
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-30 11:09:21 +0200

    memory check in main will now print the max allocated value

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,7 @@
 #include "memory/check.h"
 #include "memory/memory.h"
 #include "console/console.h"
+#include "conversions/conversions.h"
 
 #include <efi.h>
 #include <efilib.h>
@@ -10,9 +11,11 @@ EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
 {
 	InitializeLib(ImageHandle, SystemTable);
-	
-	if(!memory_check(32*1024*1024, 64*1024*1024, 1024*1024))
-		console_writeline(L"invalid parameters for memory check\n");
+
+	UINTN maxMemory = memory_check(32*1024*1024, 256*1024*1024, 8*1024*1024);
+
+	CHAR16 const * unit = convert_unit_bits_bytes(&maxMemory);
+	console_writeline(L"maximum allocated memory: %d%s", maxMemory, unit);
 
 	return EFI_SUCCESS;
 }
