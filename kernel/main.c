cf4687fe7038bd23bbe506fec1e6ead76ae76327
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-30 13:21:12 +0200

    added string allocation test

--- kernel/main.c
+++ kernel/main.c
@@ -2,11 +2,14 @@
 #include "memory/memory.h"
 #include "console/console.h"
 #include "conversions/conversions.h"
+#include "string/string.h"
 
 #include <efi.h>
 #include <efilib.h>
 
 void test_memory();
+void test_string_allocation();
+
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) 
@@ -14,6 +17,8 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 	InitializeLib(ImageHandle, SystemTable);
 
 	test_memory();
+	test_string_allocation();
+
 	return EFI_SUCCESS;
 }
 
@@ -25,4 +30,11 @@ void test_memory()
 	console_writeline(L"maximum allocated memory: %d%s", maxMemory, unit);
 }
 
+void test_string_allocation()
+{
+	CHAR16 * string = allocate_string(L"das ist ein test");
+	console_writeline(string);
+	string[0] = L'w';
+	console_writeline(string);
+	free_string(string);
 }
