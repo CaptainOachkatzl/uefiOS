1277be7a97fd4a91bd903704ae88e220ddc1aba2
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-30 11:07:54 +0200

    fixed memory check return value

--- kernel/memory/check.c
+++ kernel/memory/check.c
@@ -7,21 +7,21 @@ UINTN memory_check(UINTN min, UINTN max, UINTN step)
 	if(min == 0 || min >= max)
 		return 0;	// invalid parameters
 	
+	UINTN maxAllocated = 0;	
+	
 	int i;
 	for(i = min; i <= max; i += step)
 	{
 		void * bigarray = malloc(i);
 		if(bigarray)
 		{
-			console_writeline(L"allocated %dB of memory", i);
 			free(bigarray);
+			console_writeline(L"allocated %dB of memory", i);
+			maxAllocated = i;
 		}
 		else
 			console_writeline(L"could not allocate %dB of memory", i);
 	}
-	
-	if(i == min)
-		return 0;
 
-	return i - step;
+	return maxAllocated;
 }
